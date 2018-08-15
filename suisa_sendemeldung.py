e6d7f658b8190a1d7a557e4776f3759ae022dd06
am5z <jonas@rabe.ch>
2018-08-15 19:06:42 +0200

    refactor code, improve functionality

--- suisa_sendemeldung.py
+++ suisa_sendemeldung.py
@@ -1,107 +1,116 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 from csv import writer
-from datetime import datetime, date, timedelta
-from requests import get
+from datetime import datetime, date, timedelta, timezone
 
-# argument definition
-parser = ArgumentParser(description='ACRCloud client for SUISA reporting @ RaBe.')
-default_date = (date.today() - timedelta(days=1)).strftime("%Y%m%d")
-parser.add_argument('--access_key',
-                    help='the access key for ACRCloud (required)',
-                    required=True)
-parser.add_argument('--stream_id',
-                    help='the id of the stream at ACRCloud (required)',
-                    required=True)
-parser.add_argument('--date',
-                    help='date in format YYYYMMDD to fetch data for (defaults to yesterday)',
-                    default=default_date)
-parser.add_argument('--output',
-                    help='file to write to (defaults to <script_name>_<date>.csv)')
-args = parser.parse_args()
+import requests
 
-# sanity checks
-def print_error(msg):
-    print('ERROR: {}\n'.format(msg))
-    parser.print_help()
-    quit(1)
-if not len(args.date) == 8 or not args.date.isdigit():
-    print_error('wrong format on date')
-#if not args.stream_id.isdigit():
-#	print_error('wrong format on stream_id')
-if not len(args.access_key) == 32:
-    print_error('wrong format on access_key')
 
-stream_id=args.stream_id
-url = 'https://api.acrcloud.com/v1/monitor-streams/{}/results'.format(stream_id)
-requested_date = args.date
+class ACRClient:
 
-url_params = dict(
-    access_key=args.access_key,
-    date=requested_date 
-)
-csv_headers = [
-    'Sendedatum',
-    'Sendezeit',
-    'Sendedauer',
-    'Titel',
-    'Autoren',
-    'Interpreten',
-    'ISRC',
-    'Label'
-]
+    def __init__(self, access_key):
+        self.access_key = access_key
+        self.default_date = date.today()-timedelta(days=1)
+        self.url = ('https://api.acrcloud.com/v1/'
+                    'monitor-streams/{stream_id}/results')
 
-# fetch the data from the api and check status code
-response = get(url=url, params=url_params)
-if not response.status_code == 200:
-    print('got bad response from server')
-    print('status_code: {}'.format(response.status_code))
-    print(response.text)
-    quit(1)
+    def get_data(self, stream_id, requested_date=None):
+        if requested_date is None:
+            requested_date = self.default_date
+        url_params = dict(
+            access_key=self.access_key,
+            date=requested_date.strftime("%Y%m%d")
+        )
+        url = self.url.format(stream_id=stream_id)
+        response = requests.get(url=url, params=url_params)
+        response.raise_for_status()
+        return response.json()
 
-data = response.json()
-# write results to filename_date.csv
-if args.output:
-    filename = args.output
-else:
-    filename = __file__.replace('.py','_{}.csv').format(args.date)
-with open(filename, 'w', newline='') as csvfile:
-    # excel compatibility (https://superuser.com/questions/606272#686415)
-    csvfile.write('sep=,\n')
 
-    csv_writer = writer(csvfile, dialect='excel')
-    csv_writer.writerow(csv_headers)
+def write_csv(filename, data):
+    header = [
+        'Sendedatum',
+        'Sendezeit',
+        'Sendedauer',
+        'Titel',
+        'Künstler',
+        'ISRC',
+        'Label'
+    ]
+    with open(filename, mode='w') as csvfile:
+        csvfile.write('sep=,\n')
 
-    for entry in data:
-        metadata = entry.get('metadata')
-        # import timestamp from metadata (comes in format "yyyy-mm-dd HH:MM:SS")
-        timestamp = datetime.strptime(metadata.get('timestamp_utc'), '%Y-%m-%d %H:%M:%S')
-        # format date, time and duration according to requirements from SUISA
-        track_date = timestamp.strftime('%d/%m/%y')
-        track_time = timestamp.strftime('%H:%M:%S')
-        duration = timedelta(seconds=metadata.get('played_duration'))
-        index = 0
-        '''
-        for music in metadata.get('music'):
+        csv_writer = writer(csvfile, dialect='excel')
+        csv_writer.writerow(header)
+
+        for entry in data:
+            metadata = entry.get('metadata')
+            # parse timestamp (is in format "yyyy-mm-dd HH:MM:SS")
+            timestamp = datetime.strptime(metadata.get('timestamp_utc'),
+                                          '%Y-%m-%d %H:%M:%S')
+            timestamp = (timestamp
+                         .replace(tzinfo=timezone.utc)
+                         .astimezone(tz=None))
+
+            date = timestamp.strftime('%d/%m/%y')
+            time = timestamp.strftime('%H:%M:%S')
+            duration = timedelta(seconds=metadata.get('played_duration'))
+
+            music = metadata.get('music')[0]
             title = music.get('title')
-            # artists are separated by comma
-            authors = ', '.join([artist.get('name') for artist in music.get('artists')]) # TODO
-            performers = ', '.join([artist.get('name') for artist in music.get('artists')])
-            isrc = music.get('external_ids').get('isrc') if len(music.get('external_ids')) > 0 else ""
+            artist = ', '.join([a.get('name') for a in music.get('artists')])
+            if len(music.get('external_ids')) > 0:
+                isrc = music.get('external_ids').get('isrc')
+            else:
+                isrc = ""
             label = music.get('label')
             score = music.get('score')
 
-            # write to the csv file
-            csv_writer.writerow([date, time, duration, title, authors, performers, isrc, label])
-        '''
-        music = metadata.get('music')[0]
-        title = music.get('title')
-        # artists are separated by comma
-        authors = ', '.join([artist.get('name') for artist in music.get('artists')]) # TODO
-        performers = ', '.join([artist.get('name') for artist in music.get('artists')])
-        isrc = music.get('external_ids').get('isrc') if len(music.get('external_ids')) > 0 else ""
-        label = music.get('label')
-        score = music.get('score')
+            csv_writer.writerow([date, time, duration, title,
+                                 artist, isrc, label])
+            if score < 50:
+                print(metadata)
+                print(score)
+                quit(1)
 
-        # write to the csv file
-        csv_writer.writerow([track_date, track_time, duration, title, authors, performers, isrc, label])
+
+if __name__ == '__main__':
+    parser = ArgumentParser(
+                description='ACRCloud client for SUISA reporting @ RaBe.')
+    default_date = (date.today() - timedelta(days=1)).strftime("%Y%m%d")
+    parser.add_argument('--access_key',
+                        help='the access key for ACRCloud (required)',
+                        required=True)
+    parser.add_argument('--stream_id',
+                        help='the id of the stream at ACRCloud (required)',
+                        required=True)
+    parser.add_argument('--date',
+                        help='date in format YYYYMMDD to fetch data for \
+                              (defaults to yesterday)',
+                        default=default_date)
+    parser.add_argument('--output',
+                        help='file to write to (defaults to \
+                             <script_name>_<date>.csv)')
+    args = parser.parse_args()
+
+    def print_error(msg):
+        print('ERROR: {}\n'.format(msg))
+        parser.print_help()
+        quit(1)
+    if not len(args.access_key) == 32:
+        print_error('wrong format on access_key')
+
+    client = ACRClient(args.access_key)
+    data = []
+
+    # calculate last day of last month
+    last = date.today().replace(day=1) - timedelta(days=1)
+    # calculate last day of month before
+    current = last.replace(day=1) - timedelta(days=1)
+    # iterate over the whole last month including last day of the month before
+    while(current <= last):
+        data = data + client.get_data(args.stream_id, requested_date=current)
+        current = current + timedelta(days=1)
+
+    write_csv(args.output, data)
+    quit(0)
