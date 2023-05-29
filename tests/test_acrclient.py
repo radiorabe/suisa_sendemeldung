0acb3436897dc61a936f29820bd674a12e0c62d7
Lucas <hairmare@rabe.ch>
2023-05-29 22:06:01 +0200

    feat: use data from minio hosted s3 buckets

--- tests/test_acrclient.py
+++ tests/test_acrclient.py
@@ -6,73 +6,60 @@ from freezegun import freeze_time
 
 from suisa_sendemeldung import acrclient
 
-_ACR_URL = "https://eu-api-v2.acrcloud.com/api/bm-cs-projects/project-id/streams/stream-id/results"
+_MINIO_RAW_URL = "http://minio.example.com/acrcloud.raw/"
 
 
 def test_init():
     """Test ACRClient.__init__."""
-    bearer_token = "secret-key"
     with freeze_time("1993-03-02"):
-        acr = acrclient.ACRClient(bearer_token)
+        acr = acrclient.ACRClient(minio_url=_MINIO_RAW_URL)
 
     assert acr.default_date == date(1993, 3, 1)
 
 
 def test_get_data():
     """Test ACRClient.get_data."""
-    bearer_token = "secret-key"
-    project_id = "project-id"
-    stream_id = "stream-id"
-    data = {"data": [{"metadata": {"timestamp_utc": "1993-03-01 13:12:00"}}]}
+    data = [{"metadata": {"timestamp_utc": "1993-03-01 13:12:00"}}]
     with freeze_time("1993-03-02"):
-        acr = acrclient.ACRClient(bearer_token)
+        acr = acrclient.ACRClient(minio_url=_MINIO_RAW_URL)
     with requests_mock.Mocker() as mock:
         mock.get(
-            _ACR_URL,
+            f"{_MINIO_RAW_URL}1993-03-01.json",
             json=data,
         )
-        acr.get_data(project_id, stream_id)
+        acr.get_data()
 
 
 def test_get_interval_data():
     """Test ACRClient.get_interval_data."""
-    bearer_token = "secret-key"
-    project_id = "project-id"
-    stream_id = "stream-id"
-    data = {"data": [{"metadata": {"timestamp_utc": "1993-03-01 13:12:00"}}]}
+    data = [{"metadata": {"timestamp_utc": "1993-03-01 13:12:00"}}]
 
     with freeze_time("1993-03-02"):
-        acr = acrclient.ACRClient(bearer_token)
+        acr = acrclient.ACRClient(minio_url=_MINIO_RAW_URL)
     with requests_mock.Mocker() as mock:
         mock.get(
-            _ACR_URL,
+            requests_mock.ANY,
             json=data,
         )
-        acr.get_interval_data(
-            project_id, stream_id, date(1993, 3, 1), date(1993, 3, 31)
-        )
+        acr.get_interval_data(date(1993, 3, 1), date(1993, 3, 31))
 
     # ahead of UTC
     with freeze_time("1993-03-02"):
-        acr = acrclient.ACRClient(bearer_token)
+        acr = acrclient.ACRClient(minio_url=_MINIO_RAW_URL, timezone="Europe/Zurich")
     with requests_mock.Mocker() as mock:
-        data["data"][0]["metadata"]["timestamp_utc"] = "1993-03-01 00:00:00"
+        data[0]["metadata"]["timestamp_utc"] = "1993-03-01 00:00:00"
         mock.get(
-            _ACR_URL,
+            requests_mock.ANY,
             json=data,
         )
-        acr.get_interval_data(
-            project_id, stream_id, date(1993, 3, 1), date(1993, 3, 31), "Europe/Zurich"
-        )
+        acr.get_interval_data(date(1993, 3, 1), date(1993, 3, 31))
 
     # behind UTC
     with freeze_time("1993-03-02"):
-        acr = acrclient.ACRClient(bearer_token)
+        acr = acrclient.ACRClient(minio_url=_MINIO_RAW_URL, timezone="America/Nuuk")
     with requests_mock.Mocker() as mock:
         mock.get(
-            _ACR_URL,
+            requests_mock.ANY,
             json=data,
         )
-        acr.get_interval_data(
-            project_id, stream_id, date(1993, 3, 1), date(1993, 3, 31), "America/Nuuk"
-        )
+        acr.get_interval_data(date(1993, 3, 1), date(1993, 3, 31))
